1ca141923ac3abc96f0734a83ae4d7677ca169be
Soap <soapunny@gmail.com>
2022-08-18 23:55:22 +0900

    hostname update

--- src/server/init.js
+++ src/server/init.js
@@ -8,8 +8,9 @@ import "../db/likeDBModel";
 import "../db/statusDBModel";
 import app from "./server.js";
 
+const HOSTNAME = "127.0.0.1";
 const PORT = process.env.PORT || 12080;
 
-app.listen(PORT, () => {
+app.listen(PORT, HOSTNAME, () => {
     console.log(`Server listening on port http://${HOSTNAME}:${PORT}`);
 });
