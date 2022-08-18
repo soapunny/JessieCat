ff1428fbc263e736c086f373cbaa499cf272bef1
Soap <soapunny@gmail.com>
2022-08-19 01:16:02 +0900

    bcryptjs

--- src/db/userDBModel.js
+++ src/db/userDBModel.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import { userLevel, userStatus } from "../names/names";
 
 const userSchema = new mongoose.Schema({
